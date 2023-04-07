a51589246cf91130786dd48fa74dd38c45551dca
andre.borba <dev_andre.filipe@outlook.com>
2023-04-07 19:38:22 -0300

    feat: add base structure with React.FC components for dashboard, list and signin

---
+++ src/pages/List/index.tsx
@@ -0,0 +1,11 @@
+import react from 'react';
+
+const List: React.FC = () => {
+  return (
+    <>
+      <h1>List</h1>
+    </>
+  );
+}
+
+export default List;
---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,11 @@
+import react from 'react';
+
+const SignIn: React.FC = () => {
+  return (
+    <>
+      <h1>SignIn</h1>
+    </>
+  );
+}
+
+export default SignIn;

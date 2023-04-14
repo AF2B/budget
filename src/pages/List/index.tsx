83e954fdd6327be7325d141b44ec63ef96d3b4aa
andre.borba <dev_andre.filipe@outlook.com>
2023-04-14 11:04:30 -0300

    feat: dynamic data

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,27 +1,71 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import { useParams } from 'react-router-dom';
 
 import ContentHeader from '../../components/ContentHeader';
 import ListCard from '../../components/ListCard';
 import SelectInput from '../../components/SelectInput';
-import { Container, Content } from './styles';
+import { Container, Content, Filters } from './styles';
 
-const List: React.FC = () => {
-  const options = [
+interface IRouteParams {
+  type: string;
+  [key: string]: string | undefined;
+}
+
+const List: React.FC<IRouteParams> = () => {
+  const { type } = useParams<IRouteParams>();
+
+  const title = useMemo(() => {
+    return type === 'entry-balance' ? 'Entradas' : 'Saídas';
+  }, [type]);
+
+  const lineColor = useMemo(() => {
+    return type === 'entry-balance' ? '#F7931B' : '#E44C4E';
+  }, [type]);
+
+  const months = [
     {
-      value: 'André',
-      label: 'Borba'
+      value: 7,
+      label: 'Julho'
     },
     {
-      value: 'Filipe',
-      label: 'Fonsêca'
+      value: 8,
+      label: 'Agosto'
+    },
+    {
+      value: 9,
+      label: 'Setembro'
+    }
+  ]
+
+  const years = [
+    {
+      value: 2021,
+      label: 2021
+    },
+    {
+      value: 2022,
+      label: 2022
+    },
+    {
+      value: 2023,
+      label: 2023
     }
   ]
 
   return (
     <Container>
-      <ContentHeader title={'Saídas'} lineColor={'#E44C4E'}>
-        <SelectInput options={options} />
+      <ContentHeader title={title} lineColor={lineColor}>
+        <SelectInput options={months} />
+        <SelectInput options={years} />
       </ContentHeader>
+      <Filters>
+        <button type="button" className='tag-filter tag-filter-recurrent'>
+          Recorrentes
+        </button>
+        <button type="button" className='tag-filter tag-filter-eventual'>
+          Eventuais
+        </button>
+      </Filters>
       <Content>
         <ListCard
           tagColor={'#E44C4E'}

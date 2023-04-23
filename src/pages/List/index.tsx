9facdade7aff77ea61d5162efb9927cd437fd75b
andre.borba <dev_andre.filipe@outlook.com>
2023-04-23 17:04:22 -0300

    feat(list): years dinamically

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -59,20 +59,25 @@ const List: React.FC<IRouteParams> = () => {
     }
   ]
 
-  const years = [
-    {
-      value: 2021,
-      label: 2021
-    },
-    {
-      value: 2022,
-      label: 2022
-    },
-    {
-      value: 2023,
-      label: 2023
-    }
-  ]
+  const years = useMemo(() => {
+    let uniqueYears: number[] = [];
+
+    listData.forEach(item => {
+      const date = new Date(item.date);
+      const year = date.getFullYear();
+
+      if(!uniqueYears.includes(year)) {
+        uniqueYears.push(year);
+      }
+    });
+
+    return uniqueYears.map(year => {
+      return {
+        value: year,
+        label: year
+      }
+    });
+  },[listData]);
 
  useEffect(() => {
   const formattedData = listData

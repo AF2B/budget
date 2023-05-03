d2112e050ae6e44c95908ea647a71b0d3ed828ce
andre.borba <dev_andre.filipe@outlook.com>
2023-05-03 14:22:09 -0300

    feat(page): signIn

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,10 +1,34 @@
-import react from 'react';
+import React from 'react';
+
+import logoImg from '../../assets/logo.svg';
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+import { Container, Form, FormTitle, Logo } from './styles';
 
 const SignIn: React.FC = () => {
   return (
-    <>
-      <h1>SignIn</h1>
-    </>
+    <Container>
+      <Logo>
+        <img src={logoImg} alt="Budget" />
+        <h3>Budget</h3>
+      </Logo>
+      <Form onSubmit={() => {}}>
+        <FormTitle>Entrar</FormTitle>
+        <Input
+          type='email'
+          placeholder='E-mail'
+          required
+        />
+        <Input
+          type='password'
+          placeholder='Senha'
+          required
+        />
+        <Button
+          type='submit'
+        />
+      </Form>
+    </Container>
   );
 }
 

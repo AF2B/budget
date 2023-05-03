2fbc2cf6580585e37377453c76468508deb402c1
andre.borba <dev_andre.filipe@outlook.com>
2023-05-03 14:22:18 -0300

    feat(route): auth

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
 import AppRoutes from './app.routes';
+import AuthRoutes from './auth.routes';
 
 const Routes: React.FC = () => (
   <BrowserRouter>
